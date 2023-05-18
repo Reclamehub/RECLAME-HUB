3f88d52940f3b1b9a9f57d8358c73d39f5728307
vishnu reclame <vishnu@reclamehub.com>
2023-05-18 23:10:19 +0530

    add nav

--- reclamehub/src/components/Navbar/Navbar.jsx
+++ reclamehub/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
+import { Box, Container, Flex, Image, Text,Button, useDisclosure,useColorModeValue,useColorMode, } from "@chakra-ui/react"
+import "./Navbar.css"
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 export const Navbar=()=>{
 
-
+    const { colorMode, toggleColorMode } = useColorMode();
 
     return(<div>
-        <h1>Navbar</h1>
+         <Container className="nav-container" maxW='100%'
+         
+         
+         bg='black' color='white'>
+<Flex className="nav-row">
+    <Box className="nav-icon-box"> <Image className="nav-icon-img" src="navicon.png" /></Box>
+    <Box className="nav-col-two-container">
+        <Flex className="nav-col-two">
+            <Box>
+            <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
+              </Box>
+            <Box><Text>About</Text> </Box>
+            <Box><Text>Services</Text></Box>
+            <Box> <Text>Explore</Text></Box>
+            <Box> <Text>Contact</Text></Box>
+        </Flex>
+    </Box>
+</Flex>
+  </Container>
     </div>)
 }
